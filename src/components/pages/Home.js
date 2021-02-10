e9282df6ffaaad80c5c4db5bc0e9f534064c9b3f
sharathreddy71 <57720044+sharathreddy71@users.noreply.github.com>
2021-02-10 22:14:04 +0100

    vishnu commmit

---
+++ src/components/pages/Home.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+import './Home.css'
+
+
+
+function Home() {
+
+    return (
+      <div class="carousel-wrapper">
+            <Carousel  infiniteLoop autoPlay 
+            showArrows={false}
+            showIndicators={false}
+            showThumbs={false}
+            showStatus={false}>
+                <div className ="image">
+                    <img src="./images/1.jpg" />
+                </div>
+                <div className ="image">
+                    <img src="./images/2.jpg" />
+                </div>
+                <div className ="image">
+                    <img src="./images/3.jpg" />
+                </div>
+            </Carousel>
+        </div>
+
+
+   )
+}
+
+export default Home
